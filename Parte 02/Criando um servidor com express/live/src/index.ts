42c5fb46ccbadf1c01b0f88f5537ca5ee22010e2
Arieleketely <arieleketely2012@hotmail.com>
2022-07-17 13:18:02 -0300

    first commit

--- Parte 02/Criando um servidor com express/live/src/index.ts
+++ Parte 02/Criando um servidor com express/live/src/index.ts
@@ -1,1 +1,11 @@
-console.log('execultei');
+import express,{Request,Response,NextFunction} from 'express';
+
+const app = express();
+
+app.get('/status',(req:Request,res:Response,next:NextFunction)=>{
+    res.status(200).send({foo:'bar'})
+
+});
+app.listen(3000,()=>{
+console.log('Aplicação execultando na porta 3000');
+});
